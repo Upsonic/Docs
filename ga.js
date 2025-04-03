d8b3165cc505d49bbbbedc24a733561b9326df2f
Onur ULUSOY <atadogan06@gmail.com>
2025-04-03 21:10:31 +0300

    Documentation edits made through Mintlify web editor

--- ga.js
+++ ga.js
@@ -1,11 +1,3 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-ESERGJL9T7"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-ESERGJL9T7');
-</script>
 
 
 <!-- Google Tag Manager -->
